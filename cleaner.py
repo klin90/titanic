66fd3ec4e0842fe6522229422b80f3ec54539d18
Kevin Lin <kevin.lin790@gmail.com>
2017-06-21 17:16:52 -0400

    6/21 Update
    Added CSV files
    Updated code to Python 3
    Ignoring local PyCharm config files

--- cleaner.py
+++ cleaner.py
@@ -1,17 +1,17 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
-import numpy as np
 
-############################
-### import and data cleaning
+# import and data cleaning
 
-train = pd.read_csv('train.csv', header = 0, index_col = 'PassengerId')
-test = pd.read_csv('test.csv', header = 0, index_col = 'PassengerId')
+train = pd.read_csv('train.csv', header=0, index_col='PassengerId')
+test = pd.read_csv('test.csv', header=0, index_col='PassengerId')
 
 # function for data cleaning
+
+
 def clean_data(df):
     # drop unused columns
-    df = df.drop(['Name', 'Ticket', 'Cabin'], axis = 1)
+    df = df.drop(['Name', 'Ticket', 'Cabin'], axis=1)
 
     # normalizes fare by passenger class: null values receive zero
     df['Fare'] = df.groupby('Pclass')['Fare'].apply(lambda x: x.sub(x.mean()))
@@ -21,10 +21,10 @@ def clean_data(df):
     df['Age'] = df.groupby(['Sex', 'Pclass'])['Age'].apply(lambda x: x.fillna(x.median()))
 
     # split sex and passenger class into dummy variables
-    df = pd.get_dummies(df, columns = ['Sex', 'Pclass', 'Embarked'])
+    df = pd.get_dummies(df, columns=['Sex', 'Pclass', 'Embarked'])
 
     # return cleaned data
-    return(df)
+    return df
 
 train = clean_data(train)
 test = clean_data(test)
@@ -34,6 +34,6 @@ train.to_csv('train2.csv')
 test.to_csv('test2.csv')
 
 # create cross-validation set and CV training set
-cvTrain, CV = train_test_split(train, test_size = 0.3)
+cvTrain, CV = train_test_split(train, test_size=0.3)
 cvTrain.to_csv('cvTrain.csv')
 CV.to_csv('cvSet.csv')
